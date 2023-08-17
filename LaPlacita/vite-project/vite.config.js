57f7ef90e85b1e432dca36e2ed623adf54ded5ba
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-08-17 12:39:37 -0300

    modifico nuevamente vite config js

--- LaPlacita/vite-project/vite.config.js
+++ LaPlacita/vite-project/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// Importar el módulo path
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +9,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'), // Usa el path correcto si tu archivo está en otra ubicación
+        main: path.resolve(__dirname, 'index.html'), // Usa el path correcto si tu archivo está en otra ubicación
       },
     },
   },
